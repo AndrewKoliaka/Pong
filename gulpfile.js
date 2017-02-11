7221bcb8ee560f2357842ebaa5e1e9bda883088f
AndrewKoliaka <andrew.koliaka@gmail.com>
2017-02-11 18:12:39 +0200

    formated folders structure & gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,44 @@
-var gulp = require('gulp'),
-    jshint = require('gulp-jshint'),
-    browserSync = require('browser-sync');
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const del = require('del');
+const concat = require('gulp-concat');
+const useref = require('gulp-useref');
 
-const baseDir = 'app/';
-gulp.task('serve', function () {
-    browserSync({
-         server: {baseDir:baseDir}
+gulp.task('serve', () => {
+    browserSync.init({
+        server: {
+            baseDir: './src'
+        }
     });
-
-    gulp.watch([baseDir + 'js/*.js', baseDir + 'index.html'],browserSync.reload)
 });
 
-gulp.task('jshint', function(){
-    gulp.src(baseDir + 'js/*.js')
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'));
+gulp.task('html', () => {
+    gulp.src('src/index.html')
+        .pipe(useref())
+        .pipe(gulp.dest('./'));
 });
 
-gulp.task('watch', function () {
-    gulp.watch(baseDir + 'js/*.js', ['jshint']);
+gulp.task('css', () => {
+    gulp.src('src/assets/*.css')
+        .pipe(concat('styles.css'))
+        .pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', ['watch', 'serve']);
+gulp.task('js', () => {
+    gulp.src('src/js/*.js')
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', () => {
+    gulp.watch('src/index.html').on('change', browserSync.reload);
+    gulp.watch('src/js/*.js').on('change', browserSync.reload);
+    gulp.watch('src/styles.css').on('change', browserSync.reload);
+});
+
+gulp.task('del', () => {
+    del(['dist', 'index.html']);
+});
+
+gulp.task('default', ['serve', 'watch']);
+gulp.task('build', ['html', 'css', 'js']);
